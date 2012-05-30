fa38b0de32a85dd60ce83f68c09c6bb0709a83e8
Stevan Little <stevan.little@iinteractive.com>
2012-05-30 00:16:44 -0400

    more talks listed

--- presentations.pl
+++ presentations.pl
@@ -2,9 +2,6 @@
 
 [
     'YAPC::NA' => {
-        2005 => [
-
-        ]
         2006 => [
             {
                 title => "Moose, it's the new Camel",
@@ -184,15 +181,91 @@
                 schedule_url => 'http://yapc2010.com/yn2010/talk/2647'
             }
         ],
-        2011 => [],
-        2012 => [],
+        2011 => [
+            {
+                title => 'Jackalope',
+                author => 'Stevan Little',
+                schedule_url => 'http://www.yapc2011.us/yn2011/talk/3252'
+            },
+            {
+                title => 'OX - The hardest working two letters in Perl',
+                author => 'Jesse Luehrs',
+                schedule_url => 'http://www.yapc2011.us/yn2011/talk/3251',
+                slide_url => 'http://tozt.net/talks/ox_yapc_na_2011/'
+            },
+            {
+                title => 'Announcing Announcements',
+                author => 'Shawn Moore',
+                schedule_url => 'http://www.yapc2011.us/yn2011/talk/3197',
+                slide_url => 'http://sartak.org/talks/yapc-na-2011/announcing-announcements/'
+            },
+            {
+                title => 'Building better applications with Data::Manager',
+                author => 'Jay Shirley',
+                schedule_url => 'http://www.yapc2011.us/yn2011/talk/3232'
+            },
+            {
+                title => 'How NOT to build a multi-million dollar eCommerce system',
+                author => 'Cory Watson',
+                schedule_url => 'http://www.yapc2011.us/yn2011/talk/3217'
+            }
+        ],
+        2012 => [
+            {
+                title => 'Tweakers Anonymous',
+                author => 'John Anderson',
+                schedule_url => 'http://act.yapcna.org/2012/talk/127'
+            },
+            {
+                title => 'Code Fast, Die Young, Throw Structured Exceptions',
+                author => 'John Anderson',
+                schedule_url => 'http://act.yapcna.org/2012/talk/126'
+            },
+            {
+                title => 'REST in the trenches - Jackalope part duex',
+                author => 'Stevan Little',
+                schedule_url => 'http://act.yapcna.org/2012/talk/35'
+            },
+            {
+                title => 'Dependency Injection with Bread::Board',
+                author => 'Jesse Luehrs',
+                schedule_url => 'http://act.yapcna.org/2012/talk/147'
+            }
+        ],
     },
     'YAPC::EU' => {
-        2007 => [],
+        2007 => [
+            {
+                title => 'Object Meta Programming',
+                author => 'Yuval Kogman',
+                schedule_url => 'http://vienna.yapceurope.org/ye2007/talk/662'
+            },
+            {
+                title => 'Introduction to Moose',
+                author => 'Stevan Little',
+                schedule_url => 'http://vienna.yapceurope.org/ye2007/talk/648'
+            }
+        ],
         2012 => [],
     },
+    'YAPC::Asia' => {},
     'OSCON' => {
-        2008 => [],
+        2008 => [
+            {
+                title => 'Moose: A Postmodern Object System for Perl 5',
+                author => 'Stevan Little',
+                schedule_url => 'http://www.oscon.com/oscon2008/public/schedule/detail/2469'
+            }
+        ],
+    },
+    'London Perl Workshop' => {
+        2011 => [
+            {
+                title => 'A Brave New Perl World',
+                author => 'Stevan Little',
+                schedule_url => 'http://conferences.yapceurope.org/lpw2011/talk/3885'
+            }
+        ]
     },
     'Pittsburgh Perl Workshop' => {
         2007 => [],
@@ -205,7 +278,18 @@
         2011 => [],
     },
     'Washington DC/Baltimore Perl Workshop' => {
-        2012 => [],
+        2012 => [
+            {
+                title => 'Future Perl (Keynote)',
+                author => 'Stevan Little',
+                schedule_url => 'http://dcbpw.org/dcbpw2012/talk/4081'
+            },
+            {
+                title => 'Introduction to Object Orientation',
+                author => 'John Anderson',
+                schedule_url => 'http://dcbpw.org/dcbpw2012/talk/4066'
+            }
+        ],
     }
     'E-LAMP Nashville' => {
         2009 => [],
