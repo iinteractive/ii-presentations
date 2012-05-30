fc8e58deb984218b08fe1c3c66e3b152abff49df
Stevan Little <stevan.little@iinteractive.com>
2012-05-29 23:59:31 -0400

    roughing out the list of presentations

---
+++ presentations.pl
@@ -0,0 +1,213 @@
+#!perl
+
+[
+    'YAPC::NA' => {
+        2005 => [
+
+        ]
+        2006 => [
+            {
+                title => "Moose, it's the new Camel",
+                author => 'Stevan Little'
+            },
+            {
+                title => 'Compiling Perl 6',
+                author => 'Yuval Kogman'
+            }
+        ],
+        2008 => [
+            {
+                title => "Moose - A post modern object system for Perl 5",
+                author => 'Stevan Little',
+                schedule_url => 'http://conferences.mongueurs.net/yn2008/talk/1061',
+            },
+            {
+                title => "Horizontal Code Resuse with Moose::Role",
+                author => 'Stevan Little',
+                schedule_url => 'http://conferences.mongueurs.net/yn2008/talk/1063',
+            }
+        ],
+        2009 => [
+            {
+                title => "KiokuDB - A Real World Introduction",
+                author => 'Stevan Little',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1914',
+            },
+            {
+                title => "To Moose or Not To Moose",
+                author => 'Stevan Little',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1941',
+            },
+            #{
+            #    title => 'Using KiokuDB',
+            #    author => 'Jonathan Rockway',
+            #    schedule_url => 'http://yapc10.org/yn2009/talk/1970',
+            #    slide_link => 'http://www.jrock.us/yapc-na-2009-kioku.esl'
+            #},
+            {
+                title => 'Extending Moose for Applications',
+                author => 'Shawn Moore',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1880',
+                slide_url => 'http://sartak.org/talks/yapc-na-2009/extending-moose/'
+            },
+            #{
+            #    title => 'XML::Toolkit: Tools to Ease the Pain',
+            #    author => 'Chris Prather',
+            #    schedule_url => 'http://yapc10.org/yn2009/talk/1932',
+            #    slide_url => 'http://chris.prather.org/talks/YAPC-2009/xml-toolkit/'
+            #},
+            {
+                title => 'What Haskell did to my brain',
+                author => 'Yuval Kogman',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1956'
+            },
+            #{
+            #    title => 'Web Applications in 2009',
+            #    author => 'Jonathan Rockway',
+            #    schedule_url => 'http://yapc10.org/yn2009/talk/1972',
+            #    slide_url => 'http://jrock.us/yapc-na-2009-web.esl.txt'
+            #},
+            #{
+            #    title => 'The EPO Extended Core',
+            #    author => 'Chris Prather',
+            #    schedule_url => 'http://yapc10.org/yn2009/talk/1931',
+            #    slide_url => 'http://chris.prather.org/talks/YAPC-2009/epo-ec/'
+            #},
+            {
+                title => 'Data Visualization with Chart::Clicker',
+                author => 'Cory Watson',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1879'
+            },
+            {
+                title => 'Moose for Managers',
+                author => 'Cory Watson',
+                schedule_url => 'http://yapc10.org/yn2009/talk/1878'
+            }
+        ],
+        2010 => [
+            {
+                title => 'Modern Perl (Tuesday Keynote)',
+                author => 'Stevan Little',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2666'
+            },
+            {
+                title => 'Intro to Moose',
+                author => 'Stevan Little',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2720',
+            },
+            {
+                title => 'Extending Moose',
+                author => 'Jesse Luehrs',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2646',
+                slide_url => 'http://tozt.net/talks/extending_moose_yapc_na_2010/'
+            },
+            {
+                title => 'Hey! Path::Dispatcher kicks ass!',
+                author => 'Shawn Moore',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2642',
+                slide_url => 'http://sartak.org/talks/yapc-na-2010/path-dispatcher/'
+            },
+            #{
+            #    title => 'Modern Perl for the Worker Pattern',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2637'
+            #},
+            #{
+            #    title => 'Introduction to POE',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2638'
+            #},
+            #{
+            #    title => 'Modern POE and Moose Integration with POEx:: Role:: SessionInstantiation',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2639'
+            #},
+            #{
+            #    title => 'POE, Plack and Web::Simple ftw',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2640'
+            #},
+            #{
+            #    title => 'Catalyst Datagrids and You',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2640'
+            #},
+            #{
+            #    title => 'Intro to Catalyst',
+            #    author => 'Nick Perez',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2739'
+            #},
+            #{
+            #    title => 'Why MooseX::POE sucks, and what to do about it',
+            #    author => 'Chris Prather',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2577'
+            #},
+            #{
+            #    title => 'Thinking in Moose',
+            #    author => 'Chris Prather',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2582'
+            #},
+            #{
+            #    title => 'Intro to Catalyst',
+            #    author => 'Chris Prather',
+            #    schedule_url => 'http://yapc2010.com/yn2010/talk/2738'
+            #},
+            {
+                title => "Template developers won't hate and designers can use.",
+                author => 'Jay Shirley',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2652'
+            },
+            {
+                title => "Building Maintainable Catalyst Applications",
+                author => 'Jay Shirley',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2653',
+                slide_url => 'http://www.slideshare.net/jshirley/zen-building-maintainable-catalyst-applications'
+            },
+            {
+                title => 'Moose for Managers: The Remix',
+                author => 'Cory Watson',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2579',
+            },
+            {
+                title => 'Invigorating The Forgotten Bits Of Your WebApp with Verification and Messaging',
+                author => 'Cory Watson',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2651'
+            },
+            {
+                title => 'Catalyst in the wild: omnihotels.com',
+                author => 'Jay Hannah',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2934',
+            },
+            {
+                title => 'App::Termcast - share your terminals!',
+                author => 'Jesse Luehrs',
+                schedule_url => 'http://yapc2010.com/yn2010/talk/2647'
+            }
+        ],
+        2011 => [],
+        2012 => [],
+    },
+    'YAPC::EU' => {
+        2007 => [],
+        2012 => [],
+    },
+    'OSCON' => {
+        2008 => [],
+    },
+    'Pittsburgh Perl Workshop' => {
+        2007 => [],
+        2008 => [],
+        2010 => [],
+        2011 => [],
+    },
+    'Orlando Perl Workshop' => {
+        2010 => [],
+        2011 => [],
+    },
+    'Washington DC/Baltimore Perl Workshop' => {
+        2012 => [],
+    }
+    'E-LAMP Nashville' => {
+        2009 => [],
+    }
+]
